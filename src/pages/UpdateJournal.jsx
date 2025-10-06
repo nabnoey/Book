2d43f05d78ea9decae7dd7315fbf734eb3855bd2
nabnoey <nabnoeyy@gmail.com>
2025-10-06 22:56:12 +0700

    update page2

--- src/pages/UpdateJournal.jsx
+++ src/pages/UpdateJournal.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import JournalService from "../services/journal.service.js";
+import JournalService from "../services/journal.service"
 import Swal from "sweetalert2";
 import { useNavigate, useParams } from "react-router";
 
